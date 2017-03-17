effd6c91aea7925af6f33fd56ed37f4bd9b2c043
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-03-17 18:59:18 -0400

    Initial Commit

---
+++ slox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slox
+//
+//  Created by Ahmad Alhashemi on 2017-03-17.
+//  Copyright © 2017 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
