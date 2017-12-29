132b24894a8408dcfb51bd561246d00d225b9b65
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-12-29 12:40:04 -0500

    Update SPM package to latest format

--- Package.swift
+++ Package.swift
@@ -1,2 +1,11 @@
+// swift-tools-version:4.0
 import PackageDescription
-let package = Package(name: "slox")
+let package = Package(
+	name: "slox",
+	targets: [
+		.target(
+			name: "slox",
+			path: "slox"
+		)
+	]
+)
